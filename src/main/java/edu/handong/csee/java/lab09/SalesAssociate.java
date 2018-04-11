4d9fd08ec0f1085a1a3e29c177fc97eee804e60c
JoJeonghoon <akima95@naver.com>
2018-04-11 23:38:20 +0900

    Add single line comments for SalesAssociate class

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -1,5 +1,5 @@
 
-package edu.handong.csee.java.lab09; 
+package edu.handong.csee.java.lab09; //package name
 
 /**
  * defines a SalesAssociate object, which has two data, name and sales.
@@ -8,14 +8,14 @@ package edu.handong.csee.java.lab09;
  * @author jo jeong hoon
  *
  */
-public class SalesAssociate {
+public class SalesAssociate { //defines SalesAssociate class
 
-	private String name;
-	private double sales;
+	private String name; //instance variable
+	private double sales; //instance variable
 
-	public SalesAssociate() {
-		name = "Null";
-		sales = 0;
+	public SalesAssociate() { //default constructor
+		name = "Null"; //set name as Null
+		sales = 0; //set sales as 0
 	}
 
 	/**
@@ -25,37 +25,37 @@ public class SalesAssociate {
 	 * @param name refers sales associate's name.
 	 * @param sales refers sales associate's sales.
 	 */
-	public SalesAssociate(String name, double sales) {
-		this.name = name;
-		this.sales = sales;
+	public SalesAssociate(String name, double sales) { //constructor
+		this.name = name; //initialize the instance variable name by input parameter
+		this.sales = sales; //initialize the instance variable sales by input parameter
 	}
 
 	/**
 	 * returns sales associate's name.
 	 * @return refers the name.
 	 */
-	public String getName() {
-		return name;
+	public String getName() { //defines getter for name
+		return name; //returns instance variable name
 	}
 	/**
 	 * sets sales associate's name which is entered.
 	 * @param name refers the entered name.
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String name) { //defines setter for name
+		this.name = name; //sets instance variable name as entered parameter value
 	}
 	/**
 	 * returns sales associate's sales.
 	 * @return refers the sale.
 	 */
-	public double getSales() {
-		return sales;
+	public double getSales() { //defines getter for sales
+		return sales; //returns instance variable sales
 	}
 	/**sets sales associate's sales which is entered.
 	 * @param sales refers the entered sales.
 	 */
-	public void setSales(double sales) {
-		this.sales = sales;
+	public void setSales(double sales) { //defines setter for sales
+		this.sales = sales; //sets instance variable sales as entered parameter value
 	}
 
 
