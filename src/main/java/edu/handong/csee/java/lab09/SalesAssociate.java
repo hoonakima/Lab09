913e8142c065983b1d9e54de8e055de770fae405
JoJeonghoon <akima95@naver.com>
2018-04-11 22:21:55 +0900

    Add SalesAssociate class

---
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -0,0 +1,33 @@
+package edu.handong.csee.java.lab09;
+
+public class SalesAssociate {
+	
+	private String name;
+	private double sales;
+	
+	public SalesAssociate() {
+		name = "Null";
+		sales = 0;
+	}
+	
+	public SalesAssociate(String name, double sales) {
+		this.name = name;
+		this.sales = sales;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSales() {
+		return sales;
+	}
+	public void setSales(double sales) {
+		this.sales = sales;
+	}
+	
+	
+
+}
